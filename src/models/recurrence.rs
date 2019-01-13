7050df3d8a8489d3336da15239b35cb5ac3ed856
Marc Dominik Migge <marcmigge@gmx.net>
2019-01-13 18:12:46 +0100

    Fix field types for recurrence model

--- src/models/recurrence.rs
+++ src/models/recurrence.rs
@@ -2,10 +2,10 @@
 pub struct Recurrence {
     pub id: i32,
     pub recurrence_type: i32,
-    pub day_of_week: i32,
-    pub day_of_month: i32,
-    pub week_of_month: i32,
-    pub day_of_year: i32,
-    pub week_of_year: i32,
-    pub month_of_year: i32,
+    pub day_of_week: Option<i32>,
+    pub day_of_month: Option<i32>,
+    pub week_of_month: Option<i32>,
+    pub day_of_year: Option<i32>,
+    pub week_of_year: Option<i32>,
+    pub month_of_year: Option<i32>,
 }
