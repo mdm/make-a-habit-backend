01fd814aab007f7cd9a9bd61b3ba301bc8574a53
Marc Dominik Migge <marcmigge@gmx.net>
2019-01-27 12:59:37 +0100

    Clean up habits controller

--- src/controllers/habits.rs
+++ src/controllers/habits.rs
@@ -1,4 +1,5 @@
 use rocket::http::Status;
+use rocket::response::status;
 use rocket_contrib::json::Json;
 use diesel::prelude::*;
 
@@ -13,14 +14,20 @@ pub fn index(db: DatabaseConnection) -> QueryResult<Json<Vec<Habit>>> {
 }
 
 #[post("/", data = "<habit>")]
-pub fn create(habit: Json<NewHabit>, db: DatabaseConnection) -> QueryResult<Json<Habit>> {
-    diesel::insert_into(habits::table)
-        .values(&habit.0)
-        .execute(&db.0)?;
+pub fn create(habit: Json<NewHabit>, db: DatabaseConnection) -> QueryResult<status::Created<Json<Habit>>> {
+    db.0.transaction(|| {
+        diesel::insert_into(habits::table)
+            .values(&habit.0)
+            .execute(&db.0)?;
 
-    habits::table.order(habits::id.desc())
-        .first(&db.0)
-        .map(|habit| Json(habit))
+        habits::table.order(habits::id.desc())
+            .first(&db.0)
+            .map(|habit: Habit| {
+                let url = uri!("/habits", read: id = habit.id).path().to_string();
+                let content = Json(habit);
+                status::Created(url, Some(content))
+            })
+    })
 }
 
 #[get("/<id>")]
@@ -32,13 +39,15 @@ pub fn read(id: i32, db: DatabaseConnection) -> QueryResult<Json<Habit>> {
 
 #[put("/<id>", data = "<habit>")]
 pub fn update(id: i32, habit: Json<NewHabit>, db: DatabaseConnection) -> QueryResult<Json<Habit>> {
-    diesel::update(habits::table.find(id))
-        .set(&habit.0)
-        .execute(&db.0)?;
+    db.0.transaction(|| {
+        diesel::update(habits::table.find(id))
+            .set(&habit.0)
+            .execute(&db.0)?;
 
-    habits::table.find(id)
-        .get_result::<Habit>(&db.0)
-        .map(|habit| Json(habit))
+        habits::table.find(id)
+            .get_result::<Habit>(&db.0)
+            .map(|habit| Json(habit))
+    })
 }
 
 #[delete("/<id>")]
