27e3f4ac515c763aa4c087908bae17758fe272b4
Marc Dominik Migge <marcmigge@gmx.net>
2019-09-01 15:16:10 +0200

    Fix migrations

--- migrations/2019-01-05-181918_create_habits/up.sql
+++ migrations/2019-01-05-181918_create_habits/up.sql
@@ -3,9 +3,11 @@ CREATE TABLE habits (
     id INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT,
+    time_limit INTEGER NOT NULL,
     start TIMESTAMP NOT NULL,
-    duration INTEGER NOT NULL,
+    next_due TIMESTAMP NOT NULL,
     done_count INTEGER NOT NULL DEFAULT 0,
-    done_streak INTEGER NOT NULL DEFAULT 0,
+    streak_current INTEGER NOT NULL DEFAULT 0,
+    streak_max INTEGER NOT NULL DEFAULT 0,
     active BOOLEAN NOT NULL DEFAULT 1
 )
--- migrations/2019-01-05-211458_create_recurrences/up.sql
+++ migrations/2019-01-05-211458_create_recurrences/up.sql
@@ -1,11 +1,13 @@
 -- Your SQL goes here
 CREATE TABLE recurrences (
     id INTEGER NOT NULL PRIMARY KEY,
+    habit_id INTEGER NOT NULL,
     recurrence_type INTEGER NOT NULL DEFAULT 0,
     day_of_week INTEGER,
     day_of_month INTEGER,
     week_of_month INTEGER,
     day_of_year INTEGER,
     week_of_year INTEGER,
-    month_of_year INTEGER
+    month_of_year INTEGER,
+    FOREIGN KEY (habit_id) REFERENCE habits (id) ON DELETE CASCADE
 )
