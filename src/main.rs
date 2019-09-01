1a3e84c6734ee9b02f423fea850d7b38022e2cdc
Marc Dominik Migge <marcmigge@gmx.net>
2019-09-01 19:51:44 +0200

    Add CORS support

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ mod errors;
 
 // use rocket_contrib::databases::diesel;
 use dotenv::dotenv;
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 #[database("habits")]
 pub struct DatabaseConnection(diesel::SqliteConnection);
@@ -20,7 +22,17 @@ pub struct DatabaseConnection(diesel::SqliteConnection);
 fn main() {
     dotenv().ok();
 
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins: AllowedOrigins::All,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::All,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors().unwrap();
+
     rocket::ignite()
+        .attach(cors)
         .attach(DatabaseConnection::fairing())
         .mount("/habits", routes![
             controllers::habits::index,
